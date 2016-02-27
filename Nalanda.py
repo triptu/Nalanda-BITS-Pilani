f21c1a4903b3b6c666577efeae29c058acabefc6
Tushar Tripathi <tushutripathi@gmail.com>
2016-02-27 20:17:25 +0530

    Update Nalanda.py

--- Nalanda.py
+++ Nalanda.py
@@ -1,6 +1,4 @@
-#this is a simple program to open all the nalanda pages of my current sem
-#courses. This saves the time to search for subjects and click on them manually.
-#you need to be login initially.
+
 
 import webbrowser
 
